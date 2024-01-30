47c51353b67e9a7882488706d9e13ff584119248
GISELLE ROSS <GISELLEROSSTECH@GMAIL.COM>
2024-01-30 16:18:58 -0500

    removed return

--- public/app.js
+++ public/app.js
@@ -1,13 +1,11 @@
 
 const readOwners = async () => {
     try{
-        const owners = await fetch("https://ownersapi.onrender.com/owners", {method:"GET"})
+        const owners = await fetch("https://ownersapi.onrender.com/owners")
         const data = await response.json();
+        console.log(data)
     } catch(err){
         console.log("Error reading owners");
-    }
-    return{
-        data
     }
 }
 readOwners();
